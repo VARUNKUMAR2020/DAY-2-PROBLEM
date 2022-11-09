06406b2e9ff943b35617bec9c13901106b3e5619
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-09 21:59:56 +0530

    Add:Palindrome or Not

--- Basic.java
+++ Basic.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Basic {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        System.out.println("Enter the number to be reversed: ");
+        System.out.println("Enter the number to be checked palindrome or not: ");
         int numbers = num.nextInt(); 
-        int reverse = 0;
+        int reverse=0;
         // we have not mentioned the initialization part of the for loop
         for (; numbers != 0; numbers = numbers / 10) {
             int remainder = numbers % 10;
             reverse = reverse * 10 + remainder;
         }
-        System.out.println("The reverse of the given number is: " + reverse);
+        if(reverse == numbers){
+            System.out.println("This is a palindrome");
+        }else{
+            System.out.println("This is not a palindrome");
+        }
+    
     }
 }
