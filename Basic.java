31864149f2ce541185d5d34399d2b15ca80b023d
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-09 17:08:40 +0530

    Add:Reverse a Given Number

--- Basic.java
+++ Basic.java
@@ -8,9 +8,20 @@ public class Basic {
         System.out.println("Enter the Number: ");
         int number = num.nextInt();
         while (number > 0) {
-            int sum = number*(number+1)/2;
-            System.out.println("The sum of natural number given is: " +sum);
+            int sum = number * (number + 1) / 2;
+            System.out.println("The sum of natural number given is: " + sum);
             break;
         }
+
+        // Reverse A Given Number
+        System.out.println("Enter The number to be reversed: ");
+        int reverse = num.nextInt();
+        int reversed = 0;
+        while (reverse != 0) {
+            int digit = reverse % 10;
+            reversed = reversed * 10 + digit;
+            reverse /= 10;
+        }
+        System.out.println("Reversed Number: " + reversed);
     }
 }
