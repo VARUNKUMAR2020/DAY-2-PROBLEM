563ea6d60e38d32f47cc989efe6844dd72111f8b
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-09 22:08:54 +0530

    Add:To check Vowel Or Consonent

--- Basic.java
+++ Basic.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Basic {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.println("Enter the number to be checked palindrome or not: ");
-        int numbers = num.nextInt(); 
-        int reverse=0;
-        // we have not mentioned the initialization part of the for loop
-        for (; numbers != 0; numbers = numbers / 10) {
-            int remainder = numbers % 10;
-            reverse = reverse * 10 + remainder;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character to check: ");
+        char ch = sc.next().charAt(0);
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
         }
-        if(reverse == numbers){
-            System.out.println("This is a palindrome");
-        }else{
-            System.out.println("This is not a palindrome");
-        }
-    
     }
 }
