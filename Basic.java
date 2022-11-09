af31e3ff5fff78b0b03058771d6c964795b73698
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-09 15:24:21 +0530

    Single Digit Number in Word

--- Basic.java
+++ Basic.java
@@ -1,8 +1,45 @@
 package com.java;
-public class Basic 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.*;
+
+public class Basic {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter The Single Digit Number: ");
+        int number = num.nextInt();
+        if (number >= 10) {
+            System.out.println("Enter The Valid Number");
+        } else {
+            if (number == 0) {
+                System.out.println("ZERO");
+            }
+            if (number == 1) {
+                System.out.println("ONE");
+            }
+            if (number == 2) {
+                System.out.println("TWO");
+            }
+            if (number == 3) {
+                System.out.println("THREE");
+            }
+            if (number == 4) {
+                System.out.println("FOUR");
+            }
+            if (number == 5) {
+                System.out.println("FIVE");
+            }
+            if (number == 6) {
+                System.out.println("SIX");
+            }
+            if (number == 7) {
+                System.out.println("SEVEN");
+            }
+            if (number == 8) {
+                System.out.println("EIGHT");
+            }
+            if (number == 9) {
+                System.out.println("NINE");
+            }
+        }
     }
 }
