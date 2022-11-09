1d2a259d727d06f84f7c742f282d68472f977bea
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-09 20:09:26 +0530

    Add:Reverse a number using For Loop

--- Basic.java
+++ Basic.java
@@ -5,23 +5,14 @@ import java.util.Scanner;
 public class Basic {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        System.out.println("Enter the Number: ");
-        int number = num.nextInt();
-        while (number > 0) {
-            int sum = number * (number + 1) / 2;
-            System.out.println("The sum of natural number given is: " + sum);
-            break;
+        System.out.println("Enter the number to be reversed: ");
+        int numbers = num.nextInt(); 
+        int reverse = 0;
+        // we have not mentioned the initialization part of the for loop
+        for (; numbers != 0; numbers = numbers / 10) {
+            int remainder = numbers % 10;
+            reverse = reverse * 10 + remainder;
         }
-
-        // Reverse A Given Number
-        System.out.println("Enter The number to be reversed: ");
-        int reverse = num.nextInt();
-        int reversed = 0;
-        while (reverse != 0) {
-            int digit = reverse % 10;
-            reversed = reversed * 10 + digit;
-            reverse /= 10;
-        }
-        System.out.println("Reversed Number: " + reversed);
+        System.out.println("The reverse of the given number is: " + reverse);
     }
 }
